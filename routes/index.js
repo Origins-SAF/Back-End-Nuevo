f1a396ead4dd8ccb3c53623c8ab9e41c9ec2eb63
EnzoEB12 <enzoexequielbrito11@gmail.com>
2023-02-23 10:55:04 -0300

    merge nhn

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ import usuarioRutas from "./usuarios.route.js"
 import loginRutas from "./auth.route.js"
 import productosRutas from "./productos.route.js"
 import parteRutas from "./parte.route.js"
+import distribuidorRutas from "./distribuidor.route.js"
+import puntoRutas from "./distribuidor.route.js"
 
 export const rutas = () => [
     inventarioRutas,
