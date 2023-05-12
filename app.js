1570e2765a8af6ea34f00fa4e94011cde4a2c8ae
EnzoEB12 <enzoexequielbrito11@gmail.com>
2023-05-12 18:43:27 -0300

    nuevo cors

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.use(morgan("dev"));
-
+app.use((req, res, next) => {
+   res.append('Access-Control-Allow-Origin', ['*']);
+   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  // res.append('Access-Control-Allow-Headers', 'Content-Type');
+   next();
+});
 
 
 // RUTAS
