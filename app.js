903b309369279176bbf5ff0978df13f084791517
EnzoEB12 <enzoexequielbrito11@gmail.com>
2023-05-12 20:57:20 -0300

    whitelist

--- app.js
+++ app.js
@@ -19,12 +19,21 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.use(morgan("dev"));
-app.use((req, res, next) => {
-   res.append('Access-Control-Allow-Origin', ['*']);
-   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-   res.append('Access-Control-Allow-Headers', 'Content-Type');
-   next();
-});
+
+const whiteList = [
+   "http://localhost:5173"
+];
+
+const corsOptions =  {
+   origin: function(origin, callback){
+      if(whiteList.indexOf(origin) !== -1 || !origin){
+         callback(null, true);
+      } else {
+         callback(new Error("Not allowed by CORS"));
+      }
+   }
+}
+app.use(cors(corsOptions))
 
 
 // RUTAS
